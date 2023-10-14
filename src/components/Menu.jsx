79f09e51f0a84c2d84e889462db31078fee4d93e
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-10-14 09:31:35 -0500

    added tailwind classes added navbar but nav is behind components

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,8 +4,9 @@ import dataArray from '../data/items.json'
 
 const Menu = () => {
   return (
-    <div>
+    <div className=''>
         <MenuItem imgUrl={dataArray[0].imgUrl} name={dataArray[0].name} bread={dataArray[0].bread} ingredients={dataArray[0].ingredients}/>
+        <MenuItem imgUrl={dataArray[1].imgUrl} name={dataArray[1].name} bread={dataArray[1].bread} ingredients={dataArray[1].ingredients}/>
     </div>
   )
 }
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,16 +3,26 @@ import React from 'react'
 const MenuItem = ({id, imgUrl ,name ,bread , ingredients}) => {
   return (
 
-    <div className='py-5 flex flex-col' > 
-        <img className='max-w-[50vh] rounded-xl' src={imgUrl} alt="" />
-        <div className='' >
-            <h2 className='text-xl font-bold' >
+    <div className='max-w-[450px] h-[450px] w-full m-auto py-16 px-4 relative group' > 
+        <img className='object-cover w-full h-full rounded-2xl bg-center bg-cover duration-500' src={imgUrl} alt="" />
+        <div className='bg-white/90 rounded-sm my-1' >
+            <h2 className='text-xl font-bold border-b border-gray-600' >
                 {name}
             </h2>
-            <h2 className='text-sm' >{bread}</h2>
+            <h2 className='text-sm border-b border-gray-600' >{bread}</h2>
             <h2>{ingredients}</h2>
         </div>
     </div>
+    // <div className='py-5 flex flex-col' > 
+    //     <img className='max-w-[148vh] rounded-sm mx-auto' src={imgUrl} alt="" />
+    //     <div className='bg-white/90 rounded-sm my-1' >
+    //         <h2 className='text-xl font-bold border-b border-gray-600' >
+    //             {name}
+    //         </h2>
+    //         <h2 className='text-sm border-b border-gray-600' >{bread}</h2>
+    //         <h2>{ingredients}</h2>
+    //     </div>
+    // </div>
   )
 }
 
