b7313831ccd107b86dfa40005ec97c543a503a05
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-10-15 13:10:43 -0500

    changed link colors and added margin top to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import favicon from '/imgs/profileFavicon1.png'
 
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center mt-3'>
         <Links />
-        <a target='_blank' href="https://kevintapolcsanyi.netlify.app/" className='text-[#00df9a] border-2 rounded-xl border-[#00df9a] scale-[80%] hover:scale-[100%] ease-in duration-200 mt-3 p-2 cursor-pointer'>Click here for a link to my Portfolio
+        <a target='_blank' href="https://kevintapolcsanyi.netlify.app/" className='text-[#00df9a] border-2 rounded-full border-[#00df9a] scale-[80%] hover:scale-[100%] ease-in duration-200 mt-3 p-3 cursor-pointer'>Click here for a link to my Portfolio
         </a>
         <div className='flex'>
           <img className='scale-[60%]' src={favicon} alt="" />
-          <h3 className='m-auto pt-2 text-xl text-[#00df9a]' >  &copy; 2023 Kevin Tapolcsanyi</h3>
+          <h3 className='m-auto pt-2 text-md text-[#00df9a]' >  &copy; 2023 Kevin Tapolcsanyi</h3>
         </div>
     </div>
   )
