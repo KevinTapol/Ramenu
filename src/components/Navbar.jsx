614fcf1960bd2f74497f8183a0d32287a08c9089
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-10-16 11:33:25 -0500

    changed favicon to grill and titles to grill menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,24 +10,24 @@ const Navbar = () => {
     }
 
   return (
-<div id='navbar' className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white relative'>
-    <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Ramsey Menu</h1>
-
-    {/* hamburger menu useState logic for open close to display hamburger or X */}
-    <div onClick={handleNav} className='flex flex-end text-[#00df9a]' style={{ position: 'fixed', top: 9, right: 9, zIndex: 999,}} >
-        {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
-    </div>
-    {/* menu is off the page by 100% to the left and pulled on by 80% when hamburger cliked */}
-    <div className={nav ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'} style={{ zIndex: 999 }}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Sandwich Menu</h1>
-        {/* create a template for array of items in the json as anchor tags adding href by item id */}
-        <div className='m-4 flex flex-col'>
-            {dataArray.map((item, index) => (
-                <a onClick={handleNav} className='flex-center text-[#00df9a] m-1' key={index} href={`#${item.id}`}>{item.name}</a>
-            ))}
+    <div id='navbar' className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white relative'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Grill Menu</h1>
+    
+        {/* hamburger menu useState logic for open close to display hamburger or X */}
+        <div onClick={handleNav} className='flex flex-end text-[#00df9a]' style={{ position: 'fixed', top: 9, right: 9, zIndex: 999,}} >
+            {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+        </div>
+        {/* menu is off the page by 100% to the left and pulled on by 80% when hamburger cliked */}
+        <div className={nav ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'} style={{ zIndex: 999 }}>
+            <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Grill Menu</h1>
+            {/* create a template for array of items in the json as anchor tags adding href by item id */}
+            <div className='m-4 flex flex-col'>
+                {dataArray.map((item, index) => (
+                    <a onClick={handleNav} className='flex-center text-[#00df9a] m-1' key={index} href={`#${item.id}`}>{item.name}</a>
+                ))}
+            </div>
         </div>
     </div>
-</div>
   )
 }
 
